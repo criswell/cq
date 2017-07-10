2fec3390ca3f364a7de80b012e9fd1f7e2cf6708
Sam Hart <hartsn@gmail.com>
2017-07-10 11:54:39 -0400

    I really am obnoxious with verbosity

--- install.sh
+++ install.sh
@@ -1,12 +1,74 @@
 #!/bin/bash
 
-unset BINPATH UNINSTALL || true
+unset BINPATH QUIET UNINSTALL || true
+
+# Shell color settings
+COLOR_RESET='\033[0m'
+COLOR_TRACE='\033[0;34m' # Blue
+COLOR_WARNING='\033[1;33m' # Yellow
+COLOR_ALERT='\033[4;31m' # Underline red
+COLOR_DIE='\033[30m\033[41m' # Red background, black text
 
 # Installation manifests
 MANIFEST_BIN=(
 `cq`
 `cq-init`
 )
+
+########################
+# HELPER FUNCTIONS
+########################
+
+# Trace functions
+inner_trace () {
+  if [ -n "$LOGFILE" ]; then
+    echo -e "$(date) $*" >> ${LOGFILE}
+  else
+    echo -e "$*"
+  fi
+}
+
+warning () {
+  if [ -n "$LOGFILE" ]; then
+    inner_trace "$*"
+  else
+    [ -n "$QUIET" ] || inner_trace "${COLOR_WARNING}$*${COLOR_RESET}"
+  fi
+}
+
+trace () {
+  if [ -n "$LOGFILE" ]; then
+    inner_trace "$*"
+  else
+    [ -n "$QUIET" ] || inner_trace "${COLOR_TRACE}$*${COLOR_RESET}"
+  fi
+}
+
+alert() {
+  if [ -n "$LOGFILE" ]; then
+    inner_trace "$*"
+  else
+    inner_trace "${COLOR_ALERT}$*${COLOR_RESET}"
+  fi
+}
+
+die() {
+  if [ -n "$LOGFILE" ]; then
+    inner_trace "$*"
+  else
+    inner_trace "${COLOR_DIE}$*${COLOR_RESET}"
+  fi
+  exit 1
+}
+
+# Installer functions
+_remove() {
+  if [ -f "$*" ]; then
+    unlink $*
+  else
+    warning "Unable to unlink '$*'"
+  fi
+}
 
 #########################
 # MAIN ENTRY POINT
@@ -24,6 +86,12 @@ usage()
   --bin=BINPATH/        Installs to BINPATH/ (defaults to '~/bin')
 
   --uninstall           Uninstall
+
+  --quiet               Perform operation quietly
+
+  --log=LOGFILE         Log installation to LOGFILE
+                        (can also be set as environmental variable
+                        "\$LOGFILE")
 
   --help                Display this help message.
 
@@ -47,6 +115,14 @@ do
       UNINSTALL=yes
       shift
       ;;
+    --quiet)
+      QUIET=yes
+      shift
+      ;;
+    --log=*)
+      LOGFILE="${i#*=}"
+      shift
+      ;;
     *)
       usage
       exit 1
@@ -56,4 +132,15 @@ do
 done
 
 [ -n "$BINPATH" ] || BINPATH=~/bin
+
+if [ -n "$UNINSTALL" ]; then
+  trace "Uninstalling cq..."
+  trace " "
+  uninstall_cq
+
+  trace "Done uninstalling"
+  exit 0
+fi
+
+install_cq
 
